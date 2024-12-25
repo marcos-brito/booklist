d82a8259f0c16c98fe7277a00473168befc6b525
Marcos Brito <marcos.brito0507@gmail.com>
2024-12-25 17:09:46 -0300

    refactor: move test setup to a regular test file

--- internal/resolvers/queries.resolvers_test.go
+++ internal/resolvers/queries.resolvers_test.go
@@ -3,12 +3,76 @@ package resolvers_test
 import (
 	"context"
 	"testing"
+	"os"
+	"time"
+    "fmt"
 
 	"github.com/marcos-brito/booklist/internal/auth"
 	"github.com/marcos-brito/booklist/internal/models"
 	"github.com/marcos-brito/booklist/internal/resolvers"
+	"github.com/joho/godotenv"
+	"github.com/marcos-brito/booklist/internal/store"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
 	ory "github.com/ory/client-go"
 )
+
+func TestMain(m *testing.M) {
+	teardown := Setup()
+    defer teardown()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func Setup() func() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	container := StartPostgres()
+	db, err := store.NewConnection()
+	if err != nil {
+		panic(err)
+	}
+
+	store.With(db)
+	store.Migrate(db)
+
+	return func() {
+		if err := testcontainers.TerminateContainer(container); err != nil {
+			panic(fmt.Sprintf("failed to terminate container: %s", err))
+		}
+	}
+}
+
+func StartPostgres() testcontainers.Container {
+	ctx := context.Background()
+	container, err := postgres.Run(ctx,
+		"postgres:16-alpine",
+		postgres.WithDatabase(os.Getenv("POSTGRES_DB")),
+		postgres.WithUsername(os.Getenv("POSTGRES_USER")),
+		postgres.WithPassword(os.Getenv("POSTGRES_PASSWORD")),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+
+	if err != nil {
+		panic(fmt.Sprintf("failed to start container: %s", err))
+	}
+
+	port, err := container.MappedPort(ctx, "5432")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Setenv("POSTGRES_PORT", port.Port())
+
+	return container
+}
 
 func TestMeResolver(t *testing.T) {
 	tests := []struct {
